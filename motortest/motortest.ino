3a8ba3a61ab304fd127af01c4079ea4b7b10c4d3
Jackie Chen <45116304+BokChoyMan@users.noreply.github.com>
2020-03-06 11:36:52 -0800

    fixed the kinda works

--- motortest/motortest.ino
+++ motortest/motortest.ino
@@ -19,7 +19,7 @@ float dot_angle_deg;
 
 float pid_p, pid_i, pid_d, PID, error;
 
-float kp=25;   //proportion**
+float kp=45;   //proportion**
 float ki=0;    //integral
 float kd=2;    //derivative**
 
@@ -40,7 +40,7 @@ float set_delay_us;
 const int max_16bit = 65536;        //Max integer 16bit can store.
 const int f_clock = 16*10^6;        //Arduino UNO clock speed is 16 MHz.
 
-int t_period_s = 800*10^(-6);       //target period to interrupt**.
+int t_period_s = 600*10^(-6);       //target period to interrupt**.
 int prescale = 256;                   //prescale factor to scale number of interrupts stored**. 
 
 const uint16_t t1_load = 0;         //resets timer back to 0.
@@ -71,8 +71,9 @@ void setup() {
   cli();          //disable global interrupts
   TCCR1A = 0;     //reset timer1 control reg A
 
-  //set prescaling value to 8
-  TCCR1B &= ~(1 << CS12);
+  //set prescaling value to 256
+  TCCR1B |= (1 << CS12);
+  TCCR1B &= ~(1 << CS11);
   TCCR1B &= ~(1 << CS10);
 
   //reset timer and set compare value
